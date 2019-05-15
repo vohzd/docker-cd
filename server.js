fedd09d98d7c37d0a5a04537c4383bcb414b865a
Ben <allobon@gmail.com>
2019-05-15 22:33:01 +0100

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 
 app.get("/", (req, res) => {
-  res.send("Go go badger racers x6");
+  res.send("Go go badger racers x7");
 });
 
 app.listen(3000, () => console.log("Running..."))
