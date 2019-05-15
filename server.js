70e2313a0b499418a77dcd9307620300705b467b
Ben <allobon@gmail.com>
2019-05-15 22:14:38 +0100

    test build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 
 app.get("/", (req, res) => {
-  res.send("Go go badger racers x5");
+  res.send("Go go badger racers x6");
 });
 
 app.listen(3000, () => console.log("Running..."))
