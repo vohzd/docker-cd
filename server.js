5e88e78c3d254a4fb9d066f6094595d92b2918e1
Ben <allobon@gmail.com>
2019-05-18 20:47:03 +0100

    test CD

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 
 app.get("/", (req, res) => {
-  res.send("Bloodsucking technology company");
+  res.send("Bloodsucking technology company. Sucking the life");
 });
 
 app.listen(3000, () => console.log("Running..."))
